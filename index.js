90d708e7e23caa9fe20e98493a33e194bc45fe70
RicardoP <picoricardo3d@outlook.com>
2023-09-22 00:42:34 -0400

    Made a basic UI that updates to rounds won and alert who won
    new styles.css was made to make a basic UI that can be interacted by
    the user
    
    the basic UI has three cards being rock paper scissors that when
    clicked start a round of rock paper scissors then show who won
    by updating the score above each player and after 5
    games have been played an alert will tell you who won or if its a draw
    
    new variables and functions where made and reworked to help
    manipulate the DOM

--- index.js
+++ index.js
@@ -1,62 +1,109 @@
 const rpsChoices = ['rock', 'paper', 'scissors'];
-// keep track of wins and loses 
-let playerWins = 0;
-let computerWins = 0;
-// get computer choice 
-// needs to be able to go pick between 3 choice at random 
-const getComputerChoice = (opt) => {
-    const PcRandomChoice = Math.floor(Math.random() * opt.length);
-    return rpsChoices[PcRandomChoice];
-}
+let playerSelection = null;
+let computerSelection = null;
+let playerScore = 0;
+let computerScore = 0;
+let gamesPlayed = 0;
+const vs = document.querySelector('.vs');
+const backToVs = vs.textContent;
 
-const playerChoice = () => {
-    let choice = prompt('Jan Ken Pon')
-    if (!rpsChoices.includes(choice.toLowerCase())) {
-        alert('Wrong Input');
-        return playerChoice();
-    } else {
-        return choice.toLowerCase();
+const rockCard = document.getElementById('rock');
+const paperCard = document.getElementById('paper');
+const scissorsCard = document.getElementById('scissors');
+
+const getComputerChoice = () => {
+    const PcRandomChoice = Math.floor(Math.random() * 3);
+    return computerSelection = rpsChoices[PcRandomChoice];
+};
+
+const playerChooseRock = () => {
+    playerSelection = 'rock';
+    playRound();
+    playerSelection = null;
+};
+
+const playerChoosePaper = () => {
+    playerSelection = 'paper';
+    playRound();
+    playerSelection = null;
+};
+
+const playerChooseScissors = () => {
+    playerSelection = 'scissors';
+    playRound();
+    playerSelection = null;
+};
+
+rockCard.addEventListener('click', playerChooseRock);
+paperCard.addEventListener('click', playerChoosePaper);
+scissorsCard.addEventListener('click', playerChooseScissors);
+
+
+const getWinner = (computerSelection) => {
+    if (playerSelection === computerSelection) return 'draw';
+    if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper') ||
+        (playerSelection === 'paper' && computerSelection === 'rock')) {
+        return 'player';
+    }
+    return 'computer';
+};
+
+const gameDecider = () => {
+    if (gamesPlayed === 5) {
+        if (playerScore > computerScore) alert('You win the series!');
+        else if (computerScore > playerScore) alert('Computer wins the series!');
+        else alert('The series is a draw!');
+        gamesPlayed = 0;
+        playerScore = 0;
+        computerScore = 0;
+        updateScore('.player-score', 0);
+        updateScore('.computer-score', 0);
     }
 };
 
-// one game of rock paper scissors
-// needs to take two parm from computer and player 
-// and retuen a sting declaring the winner and why 
-// needs to know the games rules 
-const playRound = (playerSelection, computerSelection) => {
-    if (playerSelection === 'rock' && computerSelection  === rpsChoices[2]) {
-        playerWins += 1;
-        return 'You Win! Rock beats Scissors'; 
-    } else if (playerSelection === 'paper' && computerSelection === rpsChoices[0]) {
-        playerWins += 1;
-        return 'You Win! Paper beats Rock';
-    } else if (playerSelection === 'scissors' && computerSelection === rpsChoices[1]) {
-        playerWins += 1;
-        return 'You Win! Scisscors beats Paper';
-    } else if (playerSelection === 'rock' && computerSelection === rpsChoices[1]) {
-        computerWins += 1;
-        return 'You Lose! Paper beats Rock';
-    } else if (playerSelection === 'scissors' && computerSelection === rpsChoices[0]) {
-        computerWins += 1;
-        return 'You Lose! Rock beats Scissors';
-    } else if (playerSelection === 'paper' && computerSelection === rpsChoices[2]) {
-        computerWins += 1;
-        return 'You Lose! Scissors beats Paper';
-    } else {
-        return 'Its a Tie'
+const playerWins = (winner) => {
+    if (winner === 'player') {
+        playerScore++;
+        updateScore('.player-score', playerScore)
+    };
+}
+
+const computerWins = (winner) => {
+    if (winner === 'computer') {
+        computerScore++;
+        updateScore('.computer-score', computerScore)
+    };
+}
+
+const DrawAndRevert = (winner) => {
+    if (winner === 'draw') {
+        vs.textContent = 'Draw';
+        setTimeout(function () {
+            vs.textContent = backToVs;
+        }, 1000);
     }
 }
 
-// tell the winnner of the game
-const gameDecider = () => {
-    if (playerWins > computerWins) {
-        return 'You Win!!!';
-    } else if (playerWins < computerWins) {
-        return 'You Lose!!! Better Luck next time';
-    } else {
-        return 'Tie';
-    }
+const updateScore = (score, player) => {
+    let updatedScore = document.querySelector(score).textContent = player;
+    return updatedScore;
 }
 
+const playRound = () => {
+    if (gamesPlayed < 5) {
+        const computerChoice = getComputerChoice();
+        const winner = getWinner(computerChoice);
+        playerWins(winner)
+        computerWins(winner)
+        DrawAndRevert(winner)
+        gamesPlayed++;
+        gameDecider();
+    };
+};
 
-console.log(game());
+
+
+
+
+
